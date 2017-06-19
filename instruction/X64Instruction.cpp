d3d087401e429d62415ee46d63d9ca89c4e27b28
currwin <826788949@qq.com>
2017-06-19 18:37:02 +0800

    add support for x64

--- instruction/X64Instruction.cpp
+++ instruction/X64Instruction.cpp
@@ -10,9 +10,32 @@
 #include "X64Instruction.h"
 
 bool FAHook::X64Instruction::createStub(FAHook::HookInfo *info) {
-    return false;
+    auto addr = (uint64_t)info->getOriginalAddr();
+
+    uint8_t * stub = nullptr;
+    uint32_t stubsize = 0;
+    if(addr <= 0xffffffff) {    // same as x86
+        stubsize = 6;
+        stub = new uint8_t[6];
+        stub[0] = 0x68;         // push imme
+        *(uint32_t *)&stub[1] = (uint32_t)((uint64_t)info->getHookAddr() & 0xffffffff);  // imme
+        stub[5] = 0xc3;         // retn
+
+        info->setJumpStubLen(stubsize);
+        info->setJumpStubBack(stub);
+    } else {
+        stubsize = 14;
+        stub = new uint8_t[14];
+        *(uint16_t *)&stub[0] = 0x25ff;     // jmp cs:qword imm
+        *(uint32_t *)&stub[2] = 0x00000000;
+        *(uint64_t *)&stub[6] = (uint64_t)info->getHookAddr();  //qw: imm
+    }
+    info->setJumpStubLen(stubsize);
+    info->setJumpStubBack(stub);
+    return true;
 }
 
 bool FAHook::X64Instruction::createCallOriginalStub(FAHook::HookInfo *info) {
-    return Instruction::createCallOriginalStub(info);
+    info->setCallOriginalIns(nullptr);
+    return true;
 }
--- instruction/X86Instruction.cpp
+++ instruction/X86Instruction.cpp
@@ -10,10 +10,11 @@
 #include "X86Instruction.h"
 
 bool FAHook::X86Instruction::createStub(FAHook::HookInfo *info) {
-    auto stubsize = 5;
-    uint8_t* stub = new uint8_t[5];
-    stub[0] = 0x68;         // jmp xxxx
-    *(uint32_t *)&stub[1] = (uint32_t)info->getHookAddr();
+    auto stubsize = 6;
+    uint8_t* stub = new uint8_t[6];
+    stub[0] = 0x68;         // push imme
+    *(uint32_t *)&stub[1] = (uint32_t)info->getHookAddr();  // imme
+    stub[5] = 0xc3;         // retn
 
     info->setJumpStubLen(stubsize);
     info->setJumpStubBack(stub);
